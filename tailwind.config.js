c58a38721464ff16848f26ed6e91ab3aaf8d4ba6
Hamidreza Behforouzi <behforouzir@gmail.com>
2024-05-11 11:30:15 +0330

    add config js

---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./app/**/*.{html,js}"],
+  theme: {
+    screens: {
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1440px',
+      // => @media (min-width: 1536px) { ... }
+    },
+    extend: {
+      boxShadow: {
+        'navshadow': '0 1px 0 rgba(0,0,0,.14),0 2px 0 rgba(0,0,0,.05)',
+      },
+      fontFamily:{
+        yekanbakh:['Yekan Bakh']
+      }
+    },
+  },
+  plugins: [],
+}
+
