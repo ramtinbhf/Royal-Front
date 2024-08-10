92db998708435e48f016dcf935032c4a775ea360
ramtin behforouzi <ramtin.arm@gmail.com>
2024-08-10 23:10:15 +0330

    updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,24 +7,24 @@ module.exports = {
     },
     screens: {
       'sm': '640px',
-      // => @media (min-width: 640px) { ... }
+
 
       'md': '768px',
-      // => @media (min-width: 768px) { ... }
+
 
       'lg': '1024px',
-      // => @media (min-width: 1024px) { ... }
+
 
       'xl': '1280px',
-      // => @media (min-width: 1280px) { ... }
+
 
     },
     extend: {
       boxShadow: {
         'navshadow': '0 1px 0 rgba(0,0,0,.14),0 2px 0 rgba(0,0,0,.05)',
       },
-      fontFamily:{
-        yekanbakh:['yekanbakh-fa']
+      fontFamily: {
+        yekanbakh: ['yekanbakh-fa']
       }
     },
   },
